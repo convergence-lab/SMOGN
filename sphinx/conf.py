ff7835ff4d4a921552410bf641bc4a91e7e1ceb8
Masashi Kimura <kimura@convergence-lab.com>
2019-11-09 12:46:35 +0900

    fix shpinx

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Masashi Kimura'
 author = 'Masashi Kimura'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0 alpha'
+release = 'alpha'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +30,8 @@ release = '0.0 alpha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- sphinx/index.rst
+++ sphinx/index.rst
@@ -1,18 +1,20 @@
 .. SMOGN documentation master file, created by
-   sphinx-quickstart on Sat Nov  9 11:57:29 2019.
+   sphinx-quickstart on Sat Nov  9 12:45:05 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-SMOGN's documentation!
+Welcome to SMOGN's documentation!
 =================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-   smogn
 
 
-Copyright (c) 2019. Convergence Lab.
+Indices and tables
+==================
 
-https://www.convergence-lab.com
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
